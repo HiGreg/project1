a7f3c62461ecf7f7ebedb950bb65eb15817f2f76
Greg <greg_he@outlook.com>
2016-12-21 17:39:04 +0800

    decorator class test

---
+++ day7/decorator01.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# __author__  = "Hexing"
+class Foo(object):
+    def __init__(self, func):
+    self._func = func
+
+def __call__(self):
+    print ('class decorator runing')
+    self._func()
+    print ('class decorator ending')
+
+@Foo
+def bar():
+    print ('bar')
+
+bar()
