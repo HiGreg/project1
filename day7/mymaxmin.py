038d01349a82c2914bdfec9ab9dc96009efa261a
Greg <greg_he@outlook.com>
2016-12-22 00:07:20 +0800

    decorator class test

--- day7/mymaxmin.py
+++ day7/mymaxmin.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 # __author__  = "Hexing"
+import Test01
+
 
---
+++ day7/test03.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# __author__  = "Hexing"
+from Test01 import Test01
+class Goog(Test01):
+    pass
+x  = Goog()
+x.max2(100,29)
+
+
+
+
+
