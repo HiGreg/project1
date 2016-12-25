1f57746359eddb894d0ca11eb04a8091c8dd8a38
Greg <greg_he@outlook.com>
2016-12-25 23:20:35 +0800

    add  functools  modules  wraps  ,

--- day8/decoraterfunc.py
+++ day8/decoraterfunc.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 # __author__  = "Hexing"
-
+from functools import wraps
 
 def Before(*args,**kwargs):
     print ("login sucessfully")
 def After(*args,**kwargs):
     print("this is After")
-def Find():
+def Findd():
     print ("this is final step")
 #define decorator with pamar (before, after,findd)
 def DC(*args,**kwargs):
     #define array to store func
     func_array = args
     def outer(main_func):
+        @wraps(main_func)
         def wrapper(*args,**kwargs):
             func_array[0](*args,**kwargs)
             main_func(*args,**kwargs)
